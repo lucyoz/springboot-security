58c03c7f9a06146b9d39c85fd9ef79cded8624ab
lucyoz <lucyoz.kwon@gmail.com>
2022-11-28 14:03:40 +0900

    feat: 중복체크하고 회원가입하기 기능  회원 userName(id)이 중복이면 Exception 처리

--- src/main/java/com/example/hospitalreview/domain/dto/UserDto.java
+++ src/main/java/com/example/hospitalreview/domain/dto/UserDto.java
@@ -1,11 +1,14 @@
 package com.example.hospitalreview.domain.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
+@Builder
 @AllArgsConstructor
 @Getter
 public class UserDto {
+    private Long id;
     private String userName;
     private String password;
     private String email;
--- src/main/java/com/example/hospitalreview/service/UserService.java
+++ src/main/java/com/example/hospitalreview/service/UserService.java
@@ -1,15 +1,33 @@
 package com.example.hospitalreview.service;
 
+import com.example.hospitalreview.domain.User;
 import com.example.hospitalreview.domain.dto.UserDto;
 import com.example.hospitalreview.domain.dto.UserJoinRequest;
+import com.example.hospitalreview.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
-    public UserDto join(UserJoinRequest userJoinRequest){
+    private final UserRepository userRepository;
 
-        return new UserDto("","","");
+    public UserDto join(UserJoinRequest userJoinRequest){
+        //비즈니스 로직 - 회원가입
+        //회원 userName(id) 중복 check
+        //중복이면 회원가입 x --> Exception(예외) 발생
+        userRepository.findByUserName(userJoinRequest.getUserName())
+               .ifPresent(user->new RuntimeException("해당 UserName이 중복됩니다."));
+
+        //회원가입 .save()
+        User savedUser = userRepository.save(userJoinRequest.toEntity());
+
+        return UserDto.builder()
+                .id(savedUser.getId())
+                .userName(savedUser.getUserName())
+                .email(savedUser.getEmail())
+                .build();
     }
 
 }
